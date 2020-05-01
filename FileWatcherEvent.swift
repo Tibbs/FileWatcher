ed38b3c0a522f9056b4e68e86ca79a92c9d97ea1
Tibbs <tibor@voros.com.au>
2020-05-01 16:32:22 +0930

    add eventFlagsStr

--- FileWatcherEvent.swift
+++ FileWatcherEvent.swift
@@ -65,6 +65,87 @@ extension FileWatcherEvent {
         else if self.renamed { result += " renamed" }
         else if self.modified { result += " modified" }
         else if self.xattrMod { result += " accessed" }
+        let flagsHexStr = String(self.flags, radix: 16, uppercase: true)
+        return result + "With flags:" + "0x" + flagsHexStr
+    }
+    public var eventFlagsStr: String {
+        var result = "FileWatcher eventFlags:\n"
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagNone)) != 0 {
+            result = result + "kFSEventStreamEventFlagNone" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagMustScanSubDirs)) != 0 {
+            result = result + "kFSEventStreamEventFlagMustScanSubDirs" + "\n"
+        }
+        
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagUserDropped)) != 0 {
+            result = result + "kFSEventStreamEventFlagUserDropped" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagKernelDropped)) != 0 {
+            result = result + "kFSEventStreamEventFlagKernelDropped" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagEventIdsWrapped)) != 0 {
+            result = result + "kFSEventStreamEventFlagEventIdsWrapped" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagHistoryDone)) != 0 {
+            result = result + "kFSEventStreamEventFlagHistoryDone" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagRootChanged)) != 0 {
+            result = result + "kFSEventStreamEventFlagRootChanged" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagMount)) != 0 {
+            result = result + "kFSEventStreamEventFlagMount" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagUnmount)) != 0 {
+            result = result + "kFSEventStreamEventFlagUnmount" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemChangeOwner)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemChangeOwner" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCreated)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemCreated" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemFinderInfoMod)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemFinderInfoMod" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemInodeMetaMod)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemInodeMetaMod" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsDir)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemIsDir" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsFile)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemIsFile" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsHardlink)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemIsHardlink" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsLastHardlink)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemIsLastHardlink" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemIsSymlink)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemIsSymlink" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemModified)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemModified" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRemoved)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemRemoved" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemRenamed)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemRenamed" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemXattrMod)) != 0 {
+            result = result + "kFSEventStreamEventFlagItemXattrMod" + "\n"
+        }
+        if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagOwnEvent)) != 0 {
+            result = result + "kFSEventStreamEventFlagOwnEvent" + "\n"
+        }
+        if #available(OSX 10.13, *) {
+            if (self.flags & FSEventStreamEventFlags(kFSEventStreamEventFlagItemCloned)) != 0 {
+                result = result + "kFSEventStreamEventFlagItemCloned" + "\n"
+            }
+        }
+        
         return result
     }
 }
